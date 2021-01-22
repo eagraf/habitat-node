177af64414823dc74a7d16ab6e171e9f928c187e
ethan <graf.ethan@gmail.com>
2021-01-21 20:53:36 -0500

    Added transition_test.go

---
+++ entities/transition_test.go
@@ -0,0 +1,20 @@
+package entities
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddCommunity(t *testing.T) {
+	state := InitState()
+	community := InitCommunity("community_0", "My Community", IPFS)
+
+	transition := AddCommunityTransition{
+		Community: *community,
+	}
+
+	_, err := transition.Reduce(state)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(state.Communities))
+}
