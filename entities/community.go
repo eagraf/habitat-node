b133d855e5cdec8baf8456225704a90b3a275852
ethan <graf.ethan@gmail.com>
2021-01-21 20:52:46 -0500

    Modified State and Community, and created Init functions for each

--- entities/community.go
+++ entities/community.go
@@ -5,10 +5,21 @@ type CommunityID string
 
 // A Community is a collection of users, associated with a backnet and a collection of apps
 type Community struct {
-	ID      CommunityID `json:"id"`
-	Name    string      `json:"name"`
-	Members []UserID    `json:"members"`
-	Peers   []Peer      `json:"peers"`
-	Backnet Backnet     `json:"backnet"`
-	Apps    []AppID     `json:"apps"`
+	ID      CommunityID     `json:"id"`
+	Name    string          `json:"name"`
+	Members map[UserID]User `json:"members"`
+	Peers   []Peer          `json:"peers"`
+	Backnet Backnet         `json:"backnet"`
+	Apps    []AppID         `json:"apps"`
+}
+
+func InitCommunity(id CommunityID, name string, backnetType BacknetType) *Community {
+	return &Community{
+		ID:      id,
+		Name:    name,
+		Members: make(map[UserID]User),
+		Peers:   make([]Peer, 0),
+		Backnet: *InitBacknet(backnetType),
+		Apps:    make([]AppID, 0),
+	}
 }
--- entities/state.go
+++ entities/state.go
@@ -3,6 +3,13 @@ package entities
 // State holds critical state for a node.
 // Different modules act on this struct as a state machine.
 type State struct {
-	Communities []Community `json:"communities"` // Communities that the node is helping to host
-	Users       []User      `json:"users"`       // Users that are logged into this node
+	Communities map[CommunityID]Community `json:"communities"` // Communities that the node is helping to host
+	Users       map[UserID]User           `json:"users"`       // Users that are logged into this node
+}
+
+func InitState() *State {
+	return &State{
+		Communities: make(map[CommunityID]Community),
+		Users:       make(map[UserID]User),
+	}
 }
