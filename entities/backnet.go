88a31fb15048a9013e574218d885163cd0fa8662
ethan <graf.ethan@gmail.com>
2021-01-31 22:49:44 -0500

    Added LocalBacknetConfig

--- entities/backnet.go
+++ entities/backnet.go
@@ -12,8 +12,14 @@ const (
 
 // Backnet is a "backing network", which stores files (usually in a p2p filesystem)
 type Backnet struct {
-	Type      BacknetType `json:"type"`
-	Bootstrap []string    `json:"bootstrap"`
+	Type      BacknetType        `json:"type"`
+	Bootstrap []string           `json:"bootstrap"`
+	Local     LocalBacknetConfig `json:"local_backnet_config"`
+}
+
+// LocalBacknetConfig contains configurations for a backnet that are not shared with peers
+type LocalBacknetConfig struct {
+	PortMap map[string]int
 }
 
 func InitBacknet(backnetType BacknetType) *Backnet {
