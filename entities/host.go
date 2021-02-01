68436d2d860d80cb03d559d2c11c0dff902dd336
ethan <graf.ethan@gmail.com>
2021-01-31 19:10:24 -0500

    Added HostUser entity and fixed State struct

---
+++ entities/host.go
@@ -0,0 +1,6 @@
+package entities
+
+// HostUser represents an account that is allowed to configure the physical node
+type HostUser struct {
+	Username string
+}
--- entities/state.go
+++ entities/state.go
@@ -4,12 +4,12 @@ package entities
 // Different modules act on this struct as a state machine.
 type State struct {
 	Communities map[CommunityID]Community `json:"communities"` // Communities that the node is helping to host
-	Users       map[UserID]User           `json:"users"`       // Users that are logged into this node
+	HostUsers   map[string]HostUser       `json:"users"`       // Users that are logged into this node
 }
 
 func InitState() *State {
 	return &State{
 		Communities: make(map[CommunityID]Community),
-		Users:       make(map[UserID]User),
+		HostUsers:   make(map[string]HostUser),
 	}
 }
