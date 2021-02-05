edd006ebd2a333ac3ec5e2a38e3103465538bad2
ethan <graf.ethan@gmail.com>
2021-02-04 23:49:27 -0500

    Handle configuration updates on IPFS backnets

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/ipfs/go-ipfs-config v0.11.0
+	github.com/mitchellh/mapstructure v1.4.1
 	github.com/rs/zerolog v1.20.0
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
--- orchestrator/manager.go
+++ orchestrator/manager.go
@@ -48,13 +48,11 @@ func (pm *processManager) start(state *entities.State) error {
 		var backnet Backnet
 		switch community.Backnet.Type {
 		case entities.IPFS:
-			ports := pm.allocatePorts(pid, 3)
-			myBacknet, err := InitIPFSBacknet(&community, ports[0], ports[1], ports[2])
+			myBacknet, err := InitIPFSBacknet(&community)
 			if err != nil {
 				log.Err(err).Msg("error initializing backnet")
 			}
 			backnet = myBacknet
-			log.Info().Msgf("swarm port: %d, api port: %d, gateway port: %d", ports[0], ports[1], ports[2])
 		case entities.DAT:
 			fallthrough
 		default:
@@ -99,19 +97,4 @@ func (pm *processManager) processErrorListener(process *process) {
 			err:         err,
 		}
 	}
-}
-
-func (pm *processManager) allocatePorts(processID processID, n int) []int {
-	pm.portMutex.Lock()
-	defer pm.portMutex.Unlock()
-
-	ports := make([]int, n, n)
-	for i := 0; i < n; i++ {
-		port := pm.startPort + pm.portCount + i
-		pm.portAllocs[port] = processID
-		ports[i] = port
-	}
-	pm.portCount += n
-
-	return ports
 }
