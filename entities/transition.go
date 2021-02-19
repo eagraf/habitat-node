10c3ca73296e2b09a6af274b96f1b3d0f2ab764a
ethan <graf.ethan@gmail.com>
2021-02-18 23:21:37 -0500

    Added TransitionWrapper class, which allows for Transitions to be encoded as JSON accompanied with transition type information

--- entities/transition.go
+++ entities/transition.go
@@ -1,6 +1,13 @@
 package entities
 
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/mitchellh/mapstructure"
+)
 
 // TransitionType enumerates possible state transitions
 type TransitionType string
@@ -11,6 +18,12 @@ const (
 	AddMemberTransitionType     TransitionType = "ADD_MEMBER"
 	UpdateBacknetTransitionType TransitionType = "UPDATE_BACKNET"
 )
+
+var transitionReflectionTypeRegistry = map[TransitionType]reflect.Type{
+	AddCommunityTransitionType:  reflect.TypeOf(AddCommunityTransition{}),
+	AddMemberTransitionType:     reflect.TypeOf(AddMemberTransition{}),
+	UpdateBacknetTransitionType: reflect.TypeOf(UpdateBacknetTransition{}),
+}
 
 // TransitionSubscriptionCategory enumerates different types entities that a TransitionSubscriber could be subscribed to
 type TransitionSubscriptionCategory string
@@ -34,6 +47,56 @@ type Transition interface {
 	Reduce(*State) (*State, error)
 	// TODO we might need to implement rollbacks as well
 	// TODO we might need to add a validate method
+}
+
+// TransitionWrapper adds type information to a transition in marshalled form
+type TransitionWrapper struct {
+	Type       TransitionType `json:"type"`
+	Transition Transition     `json:"transition"`
+}
+
+// UnmarhsalJSON uses reflection to extract the proper Transition type from a TransitionWrapper JSON
+func (tw *TransitionWrapper) UnmarshalJSON(bytes []byte) error {
+	// first read into map[string]interface{} to extract type information
+	var firstPass map[string]interface{}
+	err := json.Unmarshal(bytes, &firstPass)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	transitionType, ok := firstPass["type"].(string)
+	if !ok {
+		return errors.New("Transitionwrapper json has no field type")
+	}
+
+	if _, ok := firstPass["transition"]; !ok {
+		return errors.New("TransitionWrapper json has no field transition")
+	}
+
+	// Get the proper type to assert from the Transition type registry
+	reflectType, ok := transitionReflectionTypeRegistry[TransitionType(transitionType)]
+	if !ok {
+		return fmt.Errorf("type %s not in transitionReflectionTypeRegistry", transitionType)
+	}
+
+	// Create a new struct pointer that is the correct implementation of Transition
+	// and then decode into it using mapstructure
+	transitionValue := reflect.New(reflectType)
+	transition := transitionValue.Interface()
+	err = mapstructure.Decode(firstPass["transition"], transition)
+	if err != nil {
+		return err
+	}
+
+	// Write into the receiving structs fields
+	tw.Type = TransitionType(transitionType)
+	tw.Transition, ok = transition.(Transition)
+	if !ok {
+		return errors.New("unmarshalled struct is not a Transition interface")
+	}
+
+	return nil
 }
 
 // CommunityTransition is a transition to a community element in state
