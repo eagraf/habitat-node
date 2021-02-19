e8d9f942c1e74aa8061398faafec7a7211187fdd
ethan <graf.ethan@gmail.com>
2021-02-18 23:21:37 -0500

    Added TransitionWrapper class, which allows for Transitions to be encoded as JSON accompanied with transition type information

--- entities/transition_test.go
+++ entities/transition_test.go
@@ -1,10 +1,33 @@
 package entities
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestUnmarshalTransitionWrapper(t *testing.T) {
+	jsonBytes := []byte(`
+{
+	"type": "ADD_COMMUNITY",
+	"transition": {
+		"community": {
+			"id": "community_0",
+			"name": "my community"
+		}
+	}
+}
+	`)
+
+	var tw TransitionWrapper
+	err := json.Unmarshal(jsonBytes, &tw)
+	assert.Nil(t, err)
+
+	act, ok := tw.Transition.(*AddCommunityTransition)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "community_0", string(act.CommunityID()))
+}
 
 func TestAddCommunity(t *testing.T) {
 	state := InitState()
