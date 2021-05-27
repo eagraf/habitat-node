c318931c6f46f35b96de43f30902df2ad934a3df
Ethan Graf <ethan@fuzzbuzz.io>
2021-05-26 23:45:29 -0400

    Created Sequencer interface which represents a generic object that processes a sequence of transitions
    - Created TransitionSubscriberSequencer
    - Refactored core test-suite code to use Sequencers

--- test-suite/main.go
+++ test-suite/main.go
@@ -21,13 +21,51 @@ type Sequence struct {
 	Transitions []*transitions.TransitionWrapper `json:"transitions"`
 }
 
+type Sequencer interface {
+	Start() error
+	Next(*transitions.TransitionWrapper) error
+	Stop() error
+}
+
+type TransitionSubscriberSequencer struct {
+	subscriber     transitions.TransitionSubscriber
+	subscriberType string
+}
+
+func (s *TransitionSubscriberSequencer) Start() error {
+	switch s.subscriberType {
+	case ProcessManagerSubscriber:
+		state := entities.InitState()
+		s.subscriber = processes.InitManager()
+		s.subscriber.(*processes.ProcessManager).Start(state)
+	default:
+		panic(fmt.Sprintf("subsriber type %s not supported", s.subscriberType))
+	}
+
+	return nil
+}
+
+func (s *TransitionSubscriberSequencer) Next(transition *transitions.TransitionWrapper) error {
+	return s.subscriber.Receive(transition.Transition)
+}
+
+func (s *TransitionSubscriberSequencer) Stop() error {
+	switch s.subscriberType {
+	case ProcessManagerSubscriber:
+		s.subscriber.(*processes.ProcessManager).Stop()
+	default:
+		panic(fmt.Sprintf("subsriber type %s not supported", s.subscriberType))
+	}
+	return nil
+}
+
 func main() {
 
 	if len(os.Args) != 3 {
-		panic("usage: test-suite <subscriber_type> <test_file>")
+		panic("usage: test-suite <sequencer_type> <test_file>")
 	}
 
-	subscriberType := os.Args[1]
+	sequencerType := os.Args[1]
 
 	file := os.Args[2]
 	buffer, err := ioutil.ReadFile(file)
@@ -41,31 +79,32 @@ func main() {
 		panic(err)
 	}
 
-	var subscriber transitions.TransitionSubscriber
-	switch subscriberType {
+	var sequencer Sequencer
+	switch sequencerType {
 	case ProcessManagerSubscriber:
-		state := entities.InitState()
-		subscriber = processes.InitManager()
-		subscriber.(*processes.ProcessManager).Start(state)
-	default:
-		panic(fmt.Sprintf("subsriber type %s not supported", subscriberType))
+		sequencer = &TransitionSubscriberSequencer{
+			subscriberType: sequencerType,
+		}
+	}
+
+	err = sequencer.Start()
+	if err != nil {
+		panic(err)
 	}
 
 	for _, transition := range sequence.Transitions {
-		err := subscriber.Receive(transition.Transition)
+		//err := subscriber.Receive(transition.Transition)
+		err := sequencer.Next(transition)
 		if err != nil {
 			panic(err)
 		}
 	}
 
-	// Takedown if necessary
-	switch subscriberType {
-	case ProcessManagerSubscriber:
-		fmt.Println("stopping")
-		subscriber.(*processes.ProcessManager).Stop()
-	default:
-		panic(fmt.Sprintf("subsriber type %s not supported", subscriberType))
+	err = sequencer.Stop()
+	if err != nil {
+		panic(err)
 	}
+
 	// Make sure there is enought time to SIGKILL child processes
 	time.Sleep(5 * time.Second)
 }
