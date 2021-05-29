3a82758ff4cc713dfd90561eff1136d5c2b0b6b8
Ethan Graf <ethan@fuzzbuzz.io>
2021-05-29 19:02:39 -0400

    Added SequenceNumber field to TransitionWrapper

--- entities/transitions/transition_test.go
+++ entities/transitions/transition_test.go
@@ -16,13 +16,15 @@ func TestUnmarshalTransitionWrapper(t *testing.T) {
 			"id": "community_0",
 			"name": "my community"
 		}
-	}
+	},
+	"sequence_number": 1
 }
 	`)
 
 	var tw TransitionWrapper
 	err := json.Unmarshal(jsonBytes, &tw)
 	assert.Nil(t, err)
+	assert.Equal(t, uint64(1), tw.SequenceNumber)
 
 	act, ok := tw.Transition.(*AddCommunityTransition)
 	assert.Equal(t, true, ok)
